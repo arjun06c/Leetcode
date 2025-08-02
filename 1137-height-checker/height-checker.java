160042a4325153d9b16e3b29106d3403eef1afeb
arjun06c <arjun06c@gmail.com>
2025-08-02 15:36:36 +0530

    Time: 19 ms (1.91%) | Memory: 42.2 MB (9.01%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int heightChecker(int[] arr) {
+        int n=arr.length;
+     
+        int arr2[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr2[i]=arr[i];
+        }
+           Arrays.sort(arr);
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=arr2[i])
+            {  count+=1;
+             System.out.print(arr[i]+" ");
+            }
+        }
+        return count;
+    }
+}
