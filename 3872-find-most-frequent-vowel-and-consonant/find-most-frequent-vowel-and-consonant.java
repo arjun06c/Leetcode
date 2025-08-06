4ebae30170456d3d3bba8c0c54dd2ec5b983d6c8
arjun06c <arjun06c@gmail.com>
2025-08-06 14:01:22 +0530

    Time: 3 ms (58.39%) | Memory: 42.8 MB (39.72%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maxFreqSum(String s) {
         HashMap<Character,Integer> res1=new HashMap<>();
-        HashMap<Character,Integer>res2=new HashMap<>();
+     
         int max1=0;
         int max2=0;
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
-            {
+            
                 if(res1.containsKey(ch))
                 {
                  res1.put(ch,res1.get(ch)+1);
@@ -16,37 +15,26 @@ class Solution {
                 else{
                     res1.put(ch,1);
                 }
-            }
+            
         }
         for(char num:res1.keySet())
         {
-            if(res1.get(num)>max1)
+            if(num=='a'||num=='e'||num=='i'||num=='o'||num=='u')
             {
-                max1=res1.get(num);
-            }
-        }
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(ch!='a'&&ch!='e'&&ch!='i'&&ch!='o'&&ch!='u')
-            {
-                if(res2.containsKey(ch))
+                if(res1.get(num)>max1)
                 {
-                 res2.put(ch,res2.get(ch)+1);
-                }
-                else{
-                    res2.put(ch,1);
+                    max1=res1.get(num);
                 }
             }
-        }
-         for(char n:res2.keySet())
-        {
-            if(res2.get(n)>max2)
+            else if(num!='a'||num!='e'||num!='i'|| num!='o'||num!='u')
             {
-                max2=res2.get(n);
+                if(res1.get(num)>max2)
+                {
+                    max2=res1.get(num);
+                }
             }
         }
-       
+      
         return max1+max2;
     }
 }
