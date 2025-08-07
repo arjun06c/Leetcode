01e69c3784d044e88efaaf6ef12c1f964d67a6bd
arjun06c <arjun06c@gmail.com>
2025-08-07 09:46:04 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (38.69%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -7,21 +7,15 @@ class Solution {
         {
             char ch=num.charAt(i);
             k=ch-'0';
-              System.out.print(k +"");
             if(k!=0)
             {
                  ind=i;
                  break;
             }
         }
-        System.out.print(ind+"");
-        StringBuilder res=new StringBuilder();
-        for(int i=0;i<=ind;i++)
-        {
-            char ans=num.charAt(i);
-            res.append(ans);
-        }
-        return res.toString();
+    
+       
+        return num.substring(0,ind+1);
         
     }
 }
