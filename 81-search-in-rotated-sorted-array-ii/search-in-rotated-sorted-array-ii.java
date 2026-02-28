497675dfea89fe15caf3196e51360823c112b014
Arjun c <arjun06c@gmail.com>
2026-02-28 16:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (92.19%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,44 +1,40 @@
 class Solution {
     public boolean search(int[] arr, int target) {
         int n=arr.length;
-        int low=0;
-        int high=n-1;
-        int mid;
-        while(low<=high)
-        {
-            mid=low+(high-low)/2;
-            if(arr[mid]==target) return true;
-            if(arr[low]==arr[mid] && arr[mid]==arr[high]) 
+         int low=0;
+         int high=n-1;
+         while(low<=high)
+         {
+            int mid=(low+high)/2;
+            if(arr[mid]==target)return true;
+            if(arr[low]==arr[mid]&& arr[mid]==arr[high])
             {
-                low=low+1;
-                high=high-1;
+                low++;
+                high--;
                 continue;
             }
             if(arr[low]<=arr[mid])
-            {  
-                if(arr[low]<=target && target<=arr[mid])
-                {
-                 high=mid-1;
-                }
-                else
-                {
-                    low=mid+1;
-                }
-
-            }
-            else
             {
-                if(arr[mid]<=target && target<=arr[high])
-                {
-                   low=mid+1;
-                }
-                else
+                if(arr[low]<=target && arr[mid]>=target)
                 {
                     high=mid-1;
                 }
+                else{
+                    low=mid+1;
+                }
             }
-        }
+            else{
+                if(arr[mid]<=target && arr[high]>=target)
+                {
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+         }
+      return false;
 
-        return false;
+
     }
 }
