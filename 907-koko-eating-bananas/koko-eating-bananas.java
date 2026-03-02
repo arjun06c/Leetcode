7029123915201e4f2070cdcb49afd5d5ab085d9c
Arjun c <arjun06c@gmail.com>
2026-03-02 15:22:40 +0530

    Time: 26 ms (18.93%) | Memory: 48 MB (57.53%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minEatingSpeed(int[] arr, int h) {
+        int n=arr.length;
+        int low=1;
+        int high=findmax(arr,n);
+        int ans=-1;
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int totalhour=findtotal(mid,arr,n);
+            if(totalhour<=h)
+            {  ans=totalhour;
+               high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+
+
+        }
+        return low;
+    }
+    private int findmax(int arr[],int n)
+    {
+        int max=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    private int findtotal(int mid,int arr[],int n)
+    {  int total=0;
+        for(int i=0;i<n;i++)
+        {
+            total+=Math.ceil(arr[i]/(double)(mid));
+        }
+        return total;
+    }
+}
