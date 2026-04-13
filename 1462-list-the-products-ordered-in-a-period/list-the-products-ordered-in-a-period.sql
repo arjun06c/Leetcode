aa3cd6a1df319219a14408e5112f4155fa31c305
Arjun c <arjun06c@gmail.com>
2026-04-13 12:06:02 +0530

    Time: 3049 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,2 @@
+select product_name,sum(o.unit) as unit from Products p inner join Orders o
+on p.product_id=o.product_id where order_date>='2020-02-01' and o.order_date<='2020-02-29' group by o.product_id,p.product_name having unit>=100;
