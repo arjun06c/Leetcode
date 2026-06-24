ef5a82655ee89307a227abe21380a22923c856b6
Arjun c <arjun06c@gmail.com>
2026-06-24 11:58:31 +0530

    Time: 51 ms (28.45%) | Memory: 49.8 MB (75.90%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,27 +1,39 @@
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return atmost(nums,k)-atmost(nums,k-1);
-    }
-    private int atmost(int nums[],int goal)
+   
+  public int fun(int []nums,int goal)
+  {
+     int n=nums.length;
+     int left=0,right=0;
+     int count=0;
+     HashMap<Integer,Integer>res=new HashMap<>();
+     if(goal<0)
+     {
+        return 0;
+     }
+
+    while(right<n)
     {
-        int left=0,right=0,count=0;
-        int n=nums.length;
-        HashMap<Integer,Integer>res=new HashMap<>();
-        while(right<n)
+        res.put(nums[right],res.getOrDefault(nums[right],0)+1);
+        while(res.size()>goal)
         {
-            res.put(nums[right],res.getOrDefault(nums[right],0)+1);
-            while(res.size()>goal)
+            res.put(nums[left],res.get(nums[left])-1);
+            if(res.get(nums[left])==0)
             {
-               res.put(nums[left],res.get(nums[left])-1);
-               if(res.get(nums[left])==0)
-               {
-                 res.remove(nums[left]);
-               }
-               left++;
+                res.remove(nums[left]);
             }
-            count+=(right-left+1);
-            right++;
+            left++;
         }
-        return count;
+        count+=(right-left+1);
+        right++;
+    }
+return count;
+
+  }
+
+
+
+
+    public int subarraysWithKDistinct(int[] nums, int goal) {
+       return  fun(nums,goal)-fun(nums,goal-1);
     }
 }
