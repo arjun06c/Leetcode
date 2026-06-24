7c5ba69d9a4769a4ad585fa6f24b6bd58083f9cf
Arjun c <arjun06c@gmail.com>
2026-06-24 11:26:20 +0530

    Time: 10 ms (88.68%) | Memory: 59.6 MB (38.06%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,26 +1,35 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-       return atmost(nums,k)-atmost(nums,k-1);
-    }
-    private int atmost(int nums[],int goal)
-    {
-        if(goal<0)
+    public int fun(int []nums,int goal)
+  {
+      int n=nums.length;
+      int sum=0;
+      int left=0;
+      int right=0;
+       if(goal<0)
         {
-           return 0;
+            return 0;
         }
-        int left=0,right=0,count=0,sum=0;
-        int n=nums.length;
-        while(right<n)
-        {
-            sum+=nums[right]%2;
-            while(sum>goal)
-            {
-            sum-=nums[left]%2;
-            left++;
-            }
-            count+=(right-left+1);
-            right++;
-        }
-        return count;
+      int count=0;
+      while(right<n)
+      {
+       
+        sum+=nums[right]%2;
+
+       while(sum>goal)
+       {
+         sum-=nums[left]%2;
+         left++;
+       }
+       count+=(right-left+1);
+       right++;
+    
+
+
+      }
+      return count;
+  }
+
+    public int numberOfSubarrays(int[] nums, int goal) {
+        return fun(nums,goal)-fun(nums,goal-1);
     }
 }
