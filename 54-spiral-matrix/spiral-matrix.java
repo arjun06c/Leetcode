e1ae5263432406ecc21610bb0cf92b5622a2588e
Arjun c <arjun06c@gmail.com>
2026-07-25 23:45:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (80.23%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,50 +1,40 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] arr) {
-     ArrayList<Integer>res=new ArrayList<>();
-       int rlen=arr.length;
-       int clen=arr[0].length;
-       int top=0;
-       int bottom=rlen-1;
-       int left=0;
-       int right=clen-1;
-       
-       while(top<=bottom && left<=right)
-       {
-          for(int i=left;i<=right;i++)
-          {
-            res.add(arr[top][i]);
-          }
-          top++;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int top=0;
+        int bottom=matrix.length-1;
+        int left=0;
+        int right=matrix[0].length-1;
+        List<Integer>res=new ArrayList<>();
+        while(top<=bottom && left<=right)
+        {
+           for(int i=left;i<=right;i++)
+           {
+             res.add(matrix[top][i]);
+           }
+           top++;
+           for(int i=top;i<=bottom;i++)
+           {
+            res.add(matrix[i][right]);
+           }
+           right--;
+           if(top<=bottom)
+           {
+           for(int i=right;i>=left;i--)
+           {
+            res.add(matrix[bottom][i]);
+           }
+                      bottom--;
 
-          for(int i=top;i<=bottom;i++)
-          {
-            res.add(arr[i][right]);
-          }
-          right--;
-          if(top<=bottom)
-          {
-            for(int i=right;i>=left;i--)
-            {
-                res.add(arr[bottom][i]);
-            }
-          }
-          bottom--;
-          if(left<=right)
-          {
-            for(int i=bottom;i>=top;i-- )
-            {
-                res.add(arr[i][left]);
-            }
-            left++;
-          }
-
-       }
-
-
-
-       
-
-    return res;
-
+           }
+           if(left<=right)
+           {
+           for(int i=bottom;i>=top;i--)
+           {
+             res.add(matrix[i][left]);
+           }
+           left++;
+           }
+        }
+        return res;
     }
 }
