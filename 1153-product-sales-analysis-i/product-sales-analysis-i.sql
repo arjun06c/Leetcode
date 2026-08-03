0d5fe8c0efc277dc0a7c4388bf0895a8ee678297
Arjun c <arjun06c@gmail.com>
2026-08-03 21:51:58 +0530

    Time: 1640 ms (10.96%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
-select product_name,year,price
- from Product p
- join Sales s on p.product_id=s.product_id; 
+select product_name,year,price from sales s
+inner join product p
+on s.product_id=p.product_id;
